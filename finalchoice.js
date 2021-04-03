51c39720da66ea9938b369970fef5feec04c89fd
close2code-palm <78175572+close2code-palm@users.noreply.github.com>
2021-04-03 21:32:43 +0300

    radio mixin MU, button and form styles

--- finalchoice.js
+++ finalchoice.js
@@ -6,13 +6,13 @@ const currentLicense = document.querySelector('.useplan')
 const newPrice = (plan, quantity) => plan * quantity;
 
 for (let i = 0; i < radios.length; i++) {
-  radios[i].addEventListener('change', function() {
+  radios[i].addEventListener('change click', function() {
     let currsel = document.querySelector('option:selected').nodeValue;
     currentPrice ='TOTAL: ' + newPrice(currsel, this.nodeValue)
   })
 };
 
-select.addEventListener('change', function(event) {
+select.addEventListener('change click', function(event) {
   let currplan = document.querySelector('radio:selected').nodeValue;
   currentLicense.textContent = 'Selected plan:  #' + event.target.value;
   currentPrice = 'TOTAL: ' + newPrice(currplan, this.nodeValue)
